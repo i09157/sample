e66463f0228295182871293588cb7c8fbc93c7ce
manabe shinya <nantokosinuke@yahoo.co.jp>
2013-09-10 15:33:06 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tetsuzine::Application.config.secret_key_base = '17e04733240253fc739aadca0f61eca3fa66c98e1519cc386bc458761b83580f612c4a972abe53c31ff0ebe6bfec54ba36080aa2867592f8a674f4c67bc9f333'
